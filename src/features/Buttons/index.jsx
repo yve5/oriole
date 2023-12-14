58666b00dde10c20ffe5028835e1cff3fc3d0db3
Yves Roll <yves.rouille@gmail.com>
2023-12-14 17:04:29 +0100

    paging

---
+++ src/features/Buttons/index.jsx
@@ -0,0 +1,64 @@
+import Confetti from 'canvas-confetti';
+import { Fragment } from 'react';
+
+const buttons = [
+  { title: 'Primary', suffix: 'primary' },
+  { title: 'Secondary', suffix: 'outline-secondary' },
+  { title: 'Success', suffix: 'success' },
+  { title: 'Danger', suffix: 'danger' },
+  { title: 'Warning', suffix: 'warning' },
+  { title: 'Info', suffix: 'info' },
+  { title: 'Light', suffix: 'outline-light' },
+  { title: 'Dark', suffix: 'dark' },
+  { title: 'Link', suffix: 'link' },
+  // { title: 'Outline Primary', suffix: 'outline-primary' },
+  // { title: 'Outline Secondary', suffix: 'outline-secondary' },
+  // { title: 'Outline Success', suffix: 'outline-success' },
+  // { title: 'Outline Danger', suffix: 'outline-danger' },
+  // { title: 'Outline Warning', suffix: 'outline-warning' },
+  // { title: 'Outline Info', suffix: 'outline-info' },
+  // { title: 'Outline Light', suffix: 'outline-light' },
+  // { title: 'Outline Dark', suffix: 'outline-dark' },
+  // { title: 'Outline Link', suffix: 'outline-link' },
+];
+
+const Buttons = () => (
+  <div className="card">
+    <div className="card-header">Buttons</div>
+
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">
+        {buttons.map(({ title, suffix }) => (
+          <button
+            className={`btn btn-${suffix} me-2 mb-2`}
+            onClick={() =>
+              Confetti({
+                particleCount: 100,
+                origin: { y: 0.6 },
+                spread: 70,
+              })
+            }
+            type="button"
+            key={suffix}
+          >
+            {title}
+          </button>
+        ))}
+      </li>
+      <li className="list-group-item bg-light-subtle">
+        <code>
+          {buttons.map(({ title, suffix }) => (
+            <Fragment key={suffix}>
+              &lt;button className=&quot;btn btn-{suffix}&quot;
+              type=&quot;button&quot;&gt;
+              {title}&lt;/button&gt;
+              <br />
+            </Fragment>
+          ))}
+        </code>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Buttons;
---
+++ src/features/Paging/index.jsx
@@ -0,0 +1,53 @@
+const Paging = () => (
+  <div className="card mt-3">
+    <div className="card-header">Paging</div>
+
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <nav aria-label="Page navigation">
+          <ul className="pagination">
+            {['Previous', '1', '2', '3', 'Next'].map((item) => (
+              <li className="page-item" key={item}>
+                <button className="page-link" type="button">
+                  {item}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Paging;
