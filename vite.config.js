dbc221449e0e7fb3382a884d2fa72531a7a6dfe2
Yves ROUILLÉ <yves.rouille@orange.com>
2023-12-18 18:31:55 +0100

    coverage

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
   base: '/oriole',
   test: {
     globals: true,
     environment: 'jsdom',
+    coverage: {
+      ...configDefaults.coverage,
+      exclude: [...configDefaults.coverage.exclude, 'src/main.jsx'],
+    },
   },
 });
