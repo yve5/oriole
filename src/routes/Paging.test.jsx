f26859fcf43092ca3bb9503cbec17e7767347552
Yves ROUILLÉ <yves.rouille@orange.com>
2024-01-03 00:30:30 +0100

    vitest

--- src/routes/Paging.test.jsx
+++ src/routes/Paging.test.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import { expect, test, vi } from 'vitest';
 import { act, create } from 'react-test-renderer';
+import { vi } from 'vitest';
 
 import Paging from './Paging';
 
 test('should match expected snapshot', () => {
-  HTMLCanvasElement.prototype.getContext = vi.fn(() => ({
-    arc: vi.fn(),
-    beginPath: vi.fn(),
-    clearRect: vi.fn(),
-    closePath: vi.fn(),
-    fill: vi.fn(),
-    lineTo: vi.fn(),
-    moveTo: vi.fn(),
-    rotate: vi.fn(),
-    restore: vi.fn(),
-    save: vi.fn(),
-    scale: vi.fn(),
-    translate: vi.fn(),
-  }));
-
   const component = create(<Paging />);
 
   act(() => {
